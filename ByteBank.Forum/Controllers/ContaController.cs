c84d931bc0f7f98f506d83d42ac5af7aafcd1702
Regiane Martins de Brito <martins.regiane.silva@gmail.com>
2021-04-13 14:14:44 -0300

    Conclusão da Aula 2

--- ByteBank.Forum/Controllers/ContaController.cs
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -1,7 +1,11 @@
-﻿using ByteBank.Forum.ViewModels;
+﻿using ByteBank.Forum.Models;
+using ByteBank.Forum.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,8 +20,26 @@ namespace ByteBank.Forum.Controllers
         }
 
         [HttpPost]
-        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        public async Task<ActionResult> Registrar(ContaRegistrarViewModel modelo)
         {
+            if (ModelState.IsValid)
+            {
+                var dbContext = new IdentityDbContext<UsuarioAplicacao>("DefaultConnection");
+                var userStore = new UserStore<UsuarioAplicacao>(dbContext);
+                var novoUsuario = new UsuarioAplicacao();
+                var userManager = new UserManager<UsuarioAplicacao>(userStore);
+
+                novoUsuario.Email = modelo.Email;
+                novoUsuario.UserName = modelo.UserName;
+                novoUsuario.NomeCompleto = modelo.NomeCompleto;
+
+                await userManager.CreateAsync(novoUsuario, modelo.Senha);
+
+                // Podemos incluir o usuário
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Alguma coisa de errado aconteceu!
             return View();
         }
     }
